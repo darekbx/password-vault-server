4ae7800d68a71c1bf8a8c99ec5ad54ebca5a8ff0
DarekMac <darek.baranczuk@gmail.com>
2019-08-23 15:42:42 +0200

    Refactoring, added battery meter

--- ui/mainmenu.py
+++ ui/mainmenu.py
@@ -1,35 +1,50 @@
 # https://www.waveshare.com/wiki/Libraries_Installation_for_RPi
-import LCD_1in44
-import LCD_Config
-
-import Image
-import ImageDraw
-import ImageFont
-import ImageColor
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageColor
 
 class MainMenu:
 
-    def __init__(self):
+    isDebug = True
+    LCD = None
 
-        LCD = LCD_1in44.LCD()
-        
-        Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
-        LCD.LCD_Init(Lcd_ScanDir)
-        
-        image = Image.new("RGB", (LCD.width, LCD.height), "WHITE")
+    def init_LCD(self):
+        if self.isDebug:
+            self.LCD = type('Expando', (object,), {})()
+            self.LCD.width = 128
+            self.LCD.height = 64
+            self.LCD.LCD_ShowImage = self.debug_ShowImage
+        else:
+            import LCD_1in44
+            import LCD_Config
+
+            self.LCD = LCD_1in44.LCD()
+            Lcd_ScanDir = self.LCD_1in44.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
+            self.LCD.LCD_Init(Lcd_ScanDir)
+    
+    def display(self):
+        image = Image.new("RGB", (self.LCD.width, self.LCD.height), "BLACK")
         draw = ImageDraw.Draw(image)
-        
-        draw.line([(0,0),(127,0)], fill = "BLUE",width = 5)
-        draw.line([(127,0),(127,63)], fill = "BLUE",width = 5)
-        draw.line([(127,63),(0,63)], fill = "BLUE",width = 5)
-        draw.line([(0,127),(0,0)], fill = "BLUE",width = 5)
-        
-        draw.rectangle([(18,10),(110,20)],fill = "RED")
-        draw.text((33, 22), 'WaveShare ', fill = "BLUE")
 
-        LCD.LCD_ShowImage(image,0,0)
+        draw.text((33, 22), 'Hello ', fill = "WHITE")
 
-        return
+        self.display_battery(draw, 70)
 
-    def display_options(self):
-        return
+        self.LCD.LCD_ShowImage(image,0,0)
+
+    def display_battery(self, draw, battery_level):
+        """
+        Parameters
+        ----------
+        battery_level : int
+            Battery level in percent
+        """
+        color = "RED" if battery_level < 20 else "GREEN"
+        value = 16 * (battery_level / 100)
+        draw.rectangle([(104, 2), (124, 8)], outline = "WHITE") 
+        draw.rectangle([(124, 4), (126, 6)], fill = "WHITE")
+        draw.rectangle([(106, 4), (104 + value, 6)], fill = color)
+
+    def debug_ShowImage(self, image, x, y):
+        image.show()
