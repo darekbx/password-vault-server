6f582e7f25aab5555be7f99013d324a028290b82
DarekMac <darek.baranczuk@gmail.com>
2019-08-27 16:54:01 +0200

    Added hardware key hadlers, code refactoring

---
+++ ui/buttons_HW.py
@@ -0,0 +1,42 @@
+import threading
+from enum import Enum
+
+class Buttons_HW:
+
+	class Key(Enum):
+		UP = 0
+		DOWN = 1
+		CENTER = 2
+
+	joystickUp = 6
+	joystickDown = 19
+	joystickCenter = 13
+	callback = None
+
+	_is_debug = False
+
+	def __init__(self, is_debug):
+		self._is_debug = is_debug
+
+	def listen_buttons(self, callback):
+		self.callback = callback
+		if self._is_debug:
+			self.handle_key(Buttons_HW.Key.CENTER)
+		else:
+			import RPi.GPIO as GPIO
+			GPIO.setup(self.joystickUp, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+			GPIO.setup(self.joystickDown, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+			GPIO.setup(self.joystickCenter, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+			threading.Thread(target=self.buttons_worker).start()
+
+	def buttons_worker(self):
+		while True:
+			if GPIO.input(self.joystickUp) == GPIO.HIGH:
+				self.handle_key(Buttons_HW.Key.UP)
+			elif GPIO.input(self.joystickDown) == GPIO.HIGH:
+				self.handle_key(Buttons_HW.Key.DOWN)
+			elif GPIO.input(self.joystickCenter) == GPIO.HIGH:
+				self.handle_key(Buttons_HW.Key.CENTER)
+
+	def handle_key(self, key):
+		self.callback(key)
--- ui/mainmenu.py
+++ ui/mainmenu.py
@@ -4,42 +4,43 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from PIL import ImageColor
 
+from display_LCD import Display_LCD
+from buttons_HW import Buttons_HW
+
 class MainMenu:
 
-    isDebug = False
-    disp = None
+    isDebug = True
     selectedItem = 2
+    lcd = None
+    buttons = None
 
-    def init_LCD(self):
-        if self.isDebug:
-            self.disp = type('Expando', (object,), {})()
-            self.disp.width = 128
-            self.disp.height = 64
-            #self.disp.ShowImage(disp.getbuffer = self.debug_ShowImage
-        else:
-            import SH1106
+    def init(self):
+        self.lcd = Display_LCD(self.isDebug)
+        self.lcd.init()
 
-            self.disp = SH1106.SH1106()
-            self.disp.Init()
-            self.disp.clear()
-    
+        self.buttons = Buttons_HW(self.isDebug)
+        self.buttons.listen_buttons(self.handle_key)
+
     def display(self):
-        image = Image.new("RGB", (self.disp.width, self.disp.height), "BLACK")
+        image = Image.new("RGB", self.lcd.dimensions(), "WHITE")
         draw = ImageDraw.Draw(image)
 
-        self.display_battery(draw, 100, True)
+        self.display_battery(draw, 100, False)
         self.display_secrets(draw, ["Gmail", "Facebook", "Lego", "Twitter"])
 
-        self.disp.ShowImage(self.disp.getbuffer(image))
+        self.lcd.show_image(image)
+
+    def handle_key(self, key):
+        print(key)
 
     def display_secrets(self, draw, secrets):
         x = 4
         y = 12
-        fnt = ImageFont.truetype('Font.ttf', 12)
+        font = self.provide_font()
         for index, secret in enumerate(secrets):
             if self.selectedItem == index:
-                draw.ellipse((5, y + 5, 9, y + 9), fill = (70,130,180))
-            draw.text((14, y), secret, font = fnt, fill = "WHITE")
+                draw.ellipse((5, y + 5, 9, y + 9), fill = 0)
+            draw.text((14, y), secret, font = font, fill = 0)
             y += 12
 
     def display_battery(self, draw, battery_level, is_charging=False):
@@ -49,18 +50,18 @@ class MainMenu:
         battery_level : int
             Battery level in percent
         """
-        color = "RED" if battery_level < 20 else "GREEN"
         value = 18 * (battery_level / 100)
-        draw.rectangle([(104, 2), (124, 10)], outline = "WHITE") 
-        draw.rectangle([(124, 6), (126, 8)], fill = "WHITE")
-        draw.rectangle([(106, 4), (104 + value, 8)], fill = color)
+        draw.rectangle([(104, 2), (124, 10)], outline = 0) 
+        draw.rectangle([(124, 6), (126, 8)], fill = 0)
+        draw.rectangle([(106, 4), (104 + value, 8)], fill = 0)
 
         if is_charging:
             draw.line((108, 7, 114, 5, 114, 5, 114, 7, 114, 7, 120, 5), fill = "WHITE")
 
-    def debug_ShowImage(self, image, x, y):
-        image.show()
+    def provide_font(self):
+        return ImageFont.truetype('Font.ttf', 9)
+
 
 m = MainMenu()
-m.init_LCD()
+m.init()
 m.display()
