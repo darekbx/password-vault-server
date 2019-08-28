0af500254970374c0b91a232f54cf24d8795bee4
DarekMac <darek.baranczuk@gmail.com>
2019-08-28 12:55:57 +0200

    Added fonts for tests

--- ui/mainmenu.py
+++ ui/mainmenu.py
@@ -9,7 +9,7 @@ from buttons_HW import Buttons_HW
 
 class MainMenu:
 
-    isDebug = False
+    isDebug = True
     selectedItem = 2
     lcd = None
     buttons = None
@@ -26,12 +26,21 @@ class MainMenu:
         draw = ImageDraw.Draw(image)
 
         self.display_battery(draw, 100, False)
-        self.display_secrets(draw, ["Gmail", "Facebook", "Lego", "Twitter"])
+        #self.display_secrets(draw, ["Gmail", "Facebook", "Lego", "Twitter"])
+        self.font_test(draw)
 
         self.lcd.show_image(image)
 
     def handle_key(self, key):
         print(key)
+
+    def font_test(self, draw):
+        draw.text((14, 0), "Facebook", font = ImageFont.truetype('fonts/game.ttf', 22), fill = 0)
+        draw.text((14, 12), "Facebook", font = ImageFont.truetype('fonts/mono.ttf', 12), fill = 0)
+        draw.text((14, 26), "Facebook", font = ImageFont.truetype('fonts/smash.ttf', 10), fill = 0)
+        draw.text((14, 32), "Facebook", font = ImageFont.truetype('fonts/wave.ttf', 14), fill = 0)
+        draw.text((14, 44), "Facebook", font = ImageFont.truetype('fonts/apple.ttf', 10), fill = 0)
+        draw.text((14, 52), "Facebook", font = ImageFont.truetype('fonts/pixel.ttf', 10), fill = 0)
 
     def display_secrets(self, draw, secrets):
         x = 4
@@ -59,7 +68,7 @@ class MainMenu:
             draw.line((108, 7, 114, 5, 114, 5, 114, 7, 114, 7, 120, 5), fill = "WHITE")
 
     def provide_font(self):
-        return ImageFont.truetype('Font.ttf', 7)
+        return ImageFont.truetype('fonts/Font.ttf', 18)
 
 
 m = MainMenu()
